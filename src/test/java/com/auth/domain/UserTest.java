7f3b542528e60b4b31017387b1b45688b87dc6cd
Higa <codehiga@gmail.com>
2023-01-12 22:19:16 -0300

    test: criação de User

---
+++ src/test/java/com/auth/domain/UserTest.java
@@ -0,0 +1,39 @@
+package com.auth.domain;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.auth.domain.entities.User;
+
+public class UserTest {
+  @Test
+  public void shouldCreateUser() {
+    String inputValidUsername = "valid_username";
+    String inputValidPassword = "12345678";
+    User createdUser = User.create(inputValidUsername, inputValidPassword);
+    assertEquals(createdUser.username.value, inputValidUsername);
+  }
+
+  @Test
+  public void shouldThrowErrorIfUsernameIsInvalid() {
+    String inputInvalidUsername = "invalid username";
+    String inputValidPassword = "12345678";
+    try {
+      User.create(inputInvalidUsername, inputValidPassword);
+    } catch(Error e) {
+      assertEquals(e.getMessage(), "O nome de úsuario não cumpre os requisitos mínimos!");
+    }
+  }
+
+  @Test
+  public void shouldThrowErrorIfPasswordIsInvalid() {
+    String inputValidUsername = "valid_username";
+    String inputInvalidPassword = "1234567";
+    try {
+      User.create(inputValidUsername, inputInvalidPassword);
+    } catch(Error e) {
+      assertEquals(e.getMessage(), "A senha não cumpre os requisitos mínimos!");
+    }
+  }
+}
