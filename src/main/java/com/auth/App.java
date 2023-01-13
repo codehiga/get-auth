1ee3f1ecfea4f9fc41339ae71d8cd18c04ae8ee7
Higa <codehiga@gmail.com>
2023-01-13 18:29:24 -0300

    chore: remoção de comentário

--- src/main/java/com/auth/App.java
+++ src/main/java/com/auth/App.java
@@ -4,11 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-/**
- * Hello world!
- *
- */
-
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
     public static void main( String[] args ) {
