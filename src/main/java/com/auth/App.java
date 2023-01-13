6b9aab16d5a21ff9080c9a7c92889f50f7d5d526
Higa <codehiga@gmail.com>
2023-01-13 17:45:35 -0300

    feat: adicionado springboot

--- src/main/java/com/auth/App.java
+++ src/main/java/com/auth/App.java
@@ -1,13 +1,17 @@
 package com.auth;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
     }
 }
