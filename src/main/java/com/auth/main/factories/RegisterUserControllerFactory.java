e68c16d79cc985d26cbda8a3f632a528a1da55cf
Higa <codehiga@gmail.com>
2023-01-13 18:30:16 -0300

    feat: factory de user controller

---
+++ src/main/java/com/auth/main/factories/RegisterUserControllerFactory.java
@@ -0,0 +1,22 @@
+package com.auth.main.factories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.auth.application.usecases.RegisterUser;
+import com.auth.domain.entities.User;
+import com.auth.infra.InMemoryUserRepository;
+import com.auth.infra.ports.UserRepository;
+import com.auth.presenters.RegisterUserController;
+
+public class RegisterUserControllerFactory {
+
+  public static RegisterUserController makeRegisterUserController() {
+    List<User> users = new ArrayList<User>();
+    UserRepository repository = new InMemoryUserRepository(users);
+    RegisterUser usecase = new RegisterUser(repository);
+    RegisterUserController controller = new RegisterUserController(usecase);
+    return controller;
+  }
+
+}
