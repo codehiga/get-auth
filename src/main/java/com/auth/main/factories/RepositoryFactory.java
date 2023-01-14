8cd09a0bf9cd69bf09a688a8555ef27256942054
higa <codehiga@gmail.com>
2023-01-14 02:37:19 -0300

    feat: factory de repository adicionado;

--- src/main/java/com/auth/main/factories/RepositoryFactory.java
+++ src/main/java/com/auth/main/factories/RepositoryFactory.java
@@ -1,9 +1,16 @@
 package com.auth.main.factories;
 
+import com.auth.domain.entities.User;
+import com.auth.infra.InMemoryUserRepository;
 import com.auth.infra.ports.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RepositoryFactory {
   public static UserRepository get() {
-    return null;
+    List<User> users = new ArrayList<User>();
+    InMemoryUserRepository inMemoryUserRepository = new InMemoryUserRepository(users);
+    return inMemoryUserRepository;
   }
 }
