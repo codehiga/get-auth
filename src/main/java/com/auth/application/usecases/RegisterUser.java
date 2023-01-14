efeb00fb56b085b067f6f868cf161c107a1867b4
higa <codehiga@gmail.com>
2023-01-14 04:20:56 -0300

    feat: coloca hash em senha quando cadastra

--- src/main/java/com/auth/application/usecases/RegisterUser.java
+++ src/main/java/com/auth/application/usecases/RegisterUser.java
@@ -1,8 +1,10 @@
 package com.auth.application.usecases;
 
 import com.auth.application.dto.CreateNewUserDTO;
+import com.auth.domain.entities.Password;
 import com.auth.domain.entities.User;
 import com.auth.domain.errors.ValidationError;
+import com.auth.infra.errors.UsernameAlreadyExistsException;
 import com.auth.infra.ports.UserRepository;
 import com.auth.shared.Either;
 
@@ -14,9 +16,14 @@ public class RegisterUser {
   }
 
   public Either<ValidationError, User> execute(CreateNewUserDTO createNewUser) {
-    Either<ValidationError, User> user = User.create(createNewUser.getUsername(), createNewUser.getPassword());
+    String hashedPassword = Password.hashPassword(createNewUser.getPassword());
+    Either<ValidationError, User> user = User.create(createNewUser.getUsername(), hashedPassword);
     if(user.isLeft()) {
       return Either.left(user.getLeft());
+    }
+    Boolean userExists = this.userRepository.exists(createNewUser.getUsername());
+    if(userExists) {
+      return Either.left(new ValidationError(ValidationError.ErrorType.USER_ALREADY_EXIST, new UsernameAlreadyExistsException().getMessage()));
     }
     Either<ValidationError, User> response = this.userRepository.save(user.getRight());
     if(response.isLeft()) {
