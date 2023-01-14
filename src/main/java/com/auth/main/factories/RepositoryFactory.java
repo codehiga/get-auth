00530d2b70319172035781e5c461288592127400
higa <codehiga@gmail.com>
2023-01-14 04:22:24 -0300

    fix: retornando NULL para repositoryFactory

--- src/main/java/com/auth/main/factories/RepositoryFactory.java
+++ src/main/java/com/auth/main/factories/RepositoryFactory.java
@@ -1,16 +1,9 @@
 package com.auth.main.factories;
 
-import com.auth.domain.entities.User;
-import com.auth.infra.InMemoryUserRepository;
 import com.auth.infra.ports.UserRepository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class RepositoryFactory {
   public static UserRepository get() {
-    List<User> users = new ArrayList<User>();
-    InMemoryUserRepository inMemoryUserRepository = new InMemoryUserRepository(users);
-    return inMemoryUserRepository;
+    return null;
   }
 }
