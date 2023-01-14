d9ca8ac283900e3cb1c0811a4e2e7f18cbefbe98
higa <codehiga@gmail.com>
2023-01-14 00:46:04 -0300

    fix: authenticate user usecase corrigido

--- src/main/java/com/auth/application/usecases/AuthenticateUser.java
+++ src/main/java/com/auth/application/usecases/AuthenticateUser.java
@@ -1,6 +1,7 @@
 package com.auth.application.usecases;
 
 import com.auth.application.dto.AuthResponseDTO;
+import com.auth.application.dto.AuthenticateUserDTO;
 import com.auth.domain.entities.Password;
 import com.auth.domain.entities.User;
 import com.auth.domain.errors.ValidationError;
@@ -16,12 +17,17 @@ public class AuthenticateUser {
     this.userRepository = userRepository;
   }
 
-  public Either<ValidationError, AuthResponseDTO> execute(User user, String password) {
+  public Either<ValidationError, AuthResponseDTO> execute(AuthenticateUserDTO authUserData) {
+    Either<ValidationError, User> userOrError = User.create(authUserData.getUsername(), authUserData.getPassword());
+    if(userOrError.isLeft()) {
+      return Either.left(new ValidationError(ErrorType.LOGIN_FAILED, userOrError.getLeft().getMessage()));
+    }
+    User user = userOrError.getRight();
     User userOrNull = this.userRepository.findByUsername(user.username.value);
     if(userOrNull.equals(null)) {
       return Either.left(new ValidationError(ErrorType.USER_NOT_FOUND, "Usuário não encontrado"));
     }
-    Boolean validPassword = Password.verifyPassword(user.password.value, password);
+    Boolean validPassword = Password.verifyPassword(user.password.value, authUserData.getPassword());
     if(validPassword.equals(false)) {
       return Either.left(new ValidationError(ErrorType.INVALID_PASSWORD, "Senha inválida."));
     }
